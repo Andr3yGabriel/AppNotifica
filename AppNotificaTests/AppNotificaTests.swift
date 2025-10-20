c05a368e4ffc8bec229291364a62b23e383a2b1c
Andrey Gonçalves <140266061+Andr3yGabriel@users.noreply.github.com>
2025-10-20 18:00:14 -0300

    Initial Commit

---
+++ AppNotificaTests/AppNotificaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppNotificaTests.swift
+//  AppNotificaTests
+//
+//  Created by Andrey Goncalves on 20/10/25.
+//
+
+import Testing
+@testable import AppNotifica
+
+struct AppNotificaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
